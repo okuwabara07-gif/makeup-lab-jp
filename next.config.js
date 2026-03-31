1c6c28db3ed89f41d0f746ebb12c0212a45b1a65
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-03-31 13:09:13 +0900

    fix: full rebuild makeup-lab-jp

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-};
-module.exports = nextConfig;
+const nextConfig = {}
+module.exports = nextConfig
